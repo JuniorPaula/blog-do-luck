96c2bac24d84a39f8dabab1657f07d5a0185b2d2
Junior Paula <slucivaldo509@gmail.com>
2021-12-14 17:29:08 -0300

    typing files from typescript

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,8 +1,11 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyles = createGlobalStyle`
-  body {
-    background: red;
+  * {
+    margin: 0;
+    padding: 0;
+    outline: none;
+    box-sizing: border-box;
   }
 
 `;
