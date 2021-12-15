2a20bfbb9aee3ac1d288ff794208e7c21845ec4b
Junior Paula <slucivaldo509@gmail.com>
2021-12-15 15:32:44 -0300

    created Header component

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -1,2 +1,2 @@
 export const URL_POSTS = 'http://localhost:1337/api/posts?populate=*';
-export const SITE_NAME = 'Luck web developer';
+export const SITE_NAME = 'BLOG do Luck';
--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -1,16 +1,21 @@
 import { PostData } from '../../domain/posts/post';
 import { Container } from './styles';
 
+import { Header } from '../../components/Header';
+
 export type HomePageProps = {
   posts: PostData[];
 };
 
 export default function HomePage({ posts }: HomePageProps) {
   return (
-    <Container>
-      {posts.map((post) => (
-        <h2 key={post.attributes.slug}>{post.attributes.title}</h2>
-      ))}
-    </Container>
+    <>
+      <Header />
+      <Container>
+        {posts.map((post) => (
+          <h2 key={post.attributes.slug}>{post.attributes.title}</h2>
+        ))}
+      </Container>
+    </>
   );
 }
